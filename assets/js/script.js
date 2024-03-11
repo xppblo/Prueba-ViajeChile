ea8d3bfe2196bdb43a5e667320ce3e6574f88dc9
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-03-11 11:31:01 -0300

    se agregan tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     $(".card-img-top").click(function () {
         $(".id-card-body").slideToggle();
     });
+
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     
 });
